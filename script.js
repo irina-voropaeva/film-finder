e779abb40fee73390aca7b76b7d9ac47649dd0ca
lunmijo <sparrow.dreamer.dog@gmail.com>
2019-06-25 21:55:16 +0300

    Added posters for the films

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 function apiSearch(event) {
     event.preventDefault(); //prevent page reloading
 
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=97dbdeb9df27f6e01c083e4a7eae4d9a&language=en-US&query=' + searchText;
-    requestAPI(server);
+    movie.innerHTML = 'Loading...';
+
+    fetch(server)
+        .then((value) => {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
+            return value.json();
+        })
+        .then((output) => {
+            let inner = '';
+            output.results.forEach(function(item) {
+                let nameItem = item.name || item.title;
+                inner += `<div class="col-3 item">
+                <img src="${urlPoster + item.poster_path}" alt="${item.name}">
+                <h5>${nameItem}</h5>
+                </div>`;
+            });
+
+            movie.innerHTML = inner;
+        })
+        .catch((reason) => {
+            movie.innerHTML = 'Sorry, something went wrong';
+            console.log('error ' + reason.status);
+        });
+
 }
 
 searchForm.addEventListener('submit', apiSearch);
-
-function requestAPI(url) {
-    const request = new XMLHttpRequest();
-
-    request.open('GET', url); //async request
-    request.send();
-
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState !== 4) {
-            movie.innerHTML = 'Loading...';
-            return;
-        }
-        if (request.status !== 200) {
-            movie.innerHTML = 'Something went wrong...';
-            console.log('Error: ' + request.status);
-            return;
-        }
-
-        const output = JSON.parse(request.responseText);
-
-        let inner = '';
-
-        output.results.forEach(function(item) {
-            let nameItem = item.name || item.title;
-            inner += `<div class="col-3">${nameItem}</div>`;
-        });
-
-        movie.innerHTML = inner;
-
-    });
-}
